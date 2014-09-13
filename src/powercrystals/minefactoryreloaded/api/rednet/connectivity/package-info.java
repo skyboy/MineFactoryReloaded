98d72184325a972be0d72bda15aa72aecbd1fd96
Parker <pyoung1998@gmail.com>
2014-09-13 18:18:56 -0400

    Added @API annotations

---
+++ src/powercrystals/minefactoryreloaded/api/rednet/connectivity/package-info.java
@@ -0,0 +1,4 @@
+@API(apiVersion = "1.0", owner = "MFRAPI|RedNet", provides = "MFRAPI|RNConnectivity")
+package powercrystals.minefactoryreloaded.api.rednet.connectivity;
+
+import cpw.mods.fml.common.API;
